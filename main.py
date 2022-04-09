994b5b917fed5f3e4672e9f9720c234ac9a2bbce
Thales Neves <thalesnevespr@gmail.com>
2022-04-09 15:42:38 -0300

    Alteração visual

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import gerar
 
 print("-=" * 30, "xXx", "=-" * 30)
-print("Olá, bem vindo ao RandomSims.")
+print("Olá, bem vindo ao RandomSims.\n")
 
 Sims = []
 while True:
-    r = input("Você deseja criar ou ler?").lower()
+    r = input("Você deseja criar ou ler?\n").lower()
     if(r == "stop"):
         break
     if(r == "criar"):
         Sims = gerar.GerarSims()
+        print("Criados com sucesso!")
     elif(r == "ler"):
         gerar.print_lista(Sims)
     else:
